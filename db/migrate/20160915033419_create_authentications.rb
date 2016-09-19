8e3d17ca4484553645480b3ae174f4484dacaf0c
Kc Lee <kc_lkc@live.com>
2016-09-19 12:21:36 +0800

    bootstrap styling

--- db/migrate/20160915033419_create_authentications.rb
+++ db/migrate/20160915033419_create_authentications.rb
@@ -2,7 +2,9 @@ class CreateAuthentications < ActiveRecord::Migration
   def change
     create_table :authentications do |t|
       t.integer :user_id
-
+      t.string :uid
+      t.string :token
+      t.string :provider
       t.timestamps null: false
     end
   end
